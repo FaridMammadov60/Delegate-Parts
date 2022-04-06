d061fecdd030c9d9e54d09d8d09da367d520e384
FaridMammadov60 <faridmma@code.edu.az>
2022-04-06 02:27:33 -0700

    add: create addbook method

--- Delegate-Part2ConsoleApp/Delegate-Part2ConsoleApp/Models/Library.cs
+++ Delegate-Part2ConsoleApp/Delegate-Part2ConsoleApp/Models/Library.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UtilsLibrary.Exceptions;
 
 namespace Delegate_Part2ConsoleApp.Models
 {
@@ -11,7 +12,68 @@ namespace Delegate_Part2ConsoleApp.Models
         #region Fields
         int _bookLimit;
         List<Book> _books;
+        int _id;
         #endregion
+
+        #region Properties
+        public List<Book> Books
+        {
+            get
+            {
+                return _books;
+            }
+            set
+            {
+                _books = value;
+            }
+        }
+        public int BookLimit 
+        {
+            get
+            {
+                return _bookLimit;
+            }
+            set
+            {
+                _bookLimit=value;
+            }
+        }
+
         public int Id { get; }
+        #endregion
+
+        #region Constructor
+        public Library()
+        {
+            _id++;
+            Id = _id;
+        }
+        #endregion
+
+        #region Methods
+        public void AddBook(Book book)
+        {
+            foreach (var item in Books)
+            {
+                if (book.IsDeleted()==false && book.Name==item.Name)
+                {
+                    AlreadyExistsException.Allready();
+                    return;
+                }
+                
+            }
+            if (Books.Count>BookLimit)
+            {
+                CapacityLimitException.CapacityLimit();
+                return;
+            }
+            Books.Add(book);
+            Console.WriteLine("Kitab uğurla Kitabxanaya əlavə edildi");
+           
+        }
+
+
+        #endregion
+
     }
 }
---
+++ Delegate-Part2ConsoleApp/UtilsLibrary/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsLibrary.Exceptions
+{
+    public static class AlreadyExistsException
+    {
+        public static void Allready()
+        {
+            Console.WriteLine("Bu kitab artıq kitabxanada var elave edilə bilməz!!!");
+        }
+    }
+}
